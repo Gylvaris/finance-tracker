b8f1692591b590c0a29dfa1481aedba1b34f9a21
Gylvaris <bellaantoni21@gmail.com>
2025-12-22 15:15:39 +0100

    Packages update

--- app/components/ExpenseChart.tsx
+++ app/components/ExpenseChart.tsx
@@ -20,7 +20,7 @@ export default function ExpenseChart({ data }: ExpenseChartProps) {
   }
 
   return (
-    <div className="h-[300px] w-full">
+    <div className="h-75 w-full">
       <ResponsiveContainer width="100%" height="100%">
         <PieChart>
           <Pie
