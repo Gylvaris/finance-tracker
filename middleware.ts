5c3cba3a0884f89a8f7d9b05d1fd525f76231c01
Gylvaris <bellaantoni21@gmail.com>
2025-12-24 19:15:37 +0100

    Feat: Add modular middleware for session handling

---
+++ middleware.ts
@@ -0,0 +1,20 @@
+import { type NextRequest } from "next/server";
+import { updateSession } from "./app/utils/supabase/middleware";
+
+export async function middleware(request: NextRequest) {
+    return await updateSession(request);
+}
+
+export const config = {
+    matcher: [
+        /*
+         * Match all request paths except:
+         * - _next/static (static files)
+         * - _next/image (image optimization files)
+         * - favicon.ico (favicon file)
+         * - /api/ (API routes)
+         * - images, svg, etc.
+         */
+        "/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)",
+    ],
+};
